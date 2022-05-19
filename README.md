189e541e10f34ffd7ebfdecc34803cb7b0f817af
valmor flores <valmorflores@gmail.com>
2022-05-19 08:13:03 -0300

    update bugfix and new tests and start docs

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# uReport
+
+Sistema de gerencia de relatórios
+Necessária a instalação do CodeIgniter 4.
+
+
+- Criar as tabelas na base de dados conforme indicado em Database (SQL)
+- Configurar app/config/Database.php
+- Configurar env.php
+
--- app/Views/Design/foot_content.php
+++ app/Views/Design/foot_content.php
@@ -1,4 +1,4 @@
-<div class="row">
+﻿<div class="row">
     <div class="col-12">
                 <div class="tile tile--center" style="box-shadow: 0 3px 6px rgba(0,0,0,0.06), 0 3px 6px rgba(0,0,0,0.03); padding: 0.25rem 1rem;">
                     <div class="row">
@@ -8,7 +8,7 @@
                         <div class="col-11">
                             <div class="tile__container">
                                 <p class="tile__title u-no-margin"><b>uRep</b> - Sistema de gestão de relatórios.</p>
-                                <span class="info">Instituto de Cardiologia - 2022/2023 - Desenvolvido específico, coordenado pela equipe da Informática</span>
+                                <span class="info">uReport: Projeto para suporte a relatórios distribuídos</span>
                             </div>
                             
                             <a href="<?php echo base_url();?>/public/about"><button type="button" class="btn btn-secondary">Sobre</button></a>
